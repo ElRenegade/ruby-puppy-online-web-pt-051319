62e575efc4db6eedf128d6c6f804293234e1867a
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-07 02:51:31 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+
+  def initialize
+    @name = name
+    @@all << name
+  end
+
+  def self.all
+    @@all.each { |dog| puts "#{dog}"  }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+  
+  
+end
